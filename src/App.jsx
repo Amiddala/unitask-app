d6583cf6df5ce6084c1f203005180fed458e55ca
kwisatzzzz <magnehinojosac@gmail.com>
2026-07-29 22:55:22 -0400

    feat(US-09): integrate group detail screen into main router

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import RegisterForm from './components/RegisterForm/RegisterForm';
 import LoginForm from './components/LoginForm/LoginForm';
 import DashboardScreen from './pages/DashboardScreen/DashboardScreen';
 import TasksScreen from './pages/TasksScreen/TasksScreen';
+import GroupDetailScreen from './screens/GroupDetailScreen'; // Importacion US-09
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import './App.css';
 
@@ -21,12 +22,21 @@ function App() {
           </ProtectedRoute>
         }
       />
-      {/* US-05, US-07, US-09 agregarán /tareas, /examenes, /grupos */}
+      {/* US-05, US-07, US-09 agregaran /tareas, /examenes, /grupos */}
       <Route
         path="/tareas"
         element={
           <ProtectedRoute>
             <TasksScreen />
+          </ProtectedRoute>
+        }
+      />
+      {/* Implementacion US-09: Vista Detallada de Grupo */}
+      <Route
+        path="/grupos/:id"
+        element={
+          <ProtectedRoute>
+            <GroupDetailScreen />
           </ProtectedRoute>
         }
       />
